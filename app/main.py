aa5793f0b4771e9b8144992e6dc2b9a47f53f880
shiquan-lim <shiquan.lim@gmail.com>
2017-02-23 12:38:51 +0800

    postgres copy command

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ hostname = 'y3optim.cnlc0eowtsp7.ap-southeast-1.rds.amazonaws.com'
 user = 'limshiq'
 password = 'awesomeSQ'
 dbname = 'y3optim'
+port = 5432
 
 def doQuery( conn ) :
     cur = conn.cursor()
@@ -15,7 +16,15 @@ def doQuery( conn ) :
     # for firstname, lastname in cur.fetchall() :
     #     print firstname, lastname
 
+def writeData(conn):
+	cur = conn.cursor()
+	f = open(r'/Users/limshiquan/Desktop/outlet.csv', 'r')
+	cur.copy_from(f, "outlets", sep=',')
+	f.close()
+
 print ("Hello world")
 myConnection = psycopg2.connect( host=hostname, user=user, password=password, dbname=dbname )
-doQuery( myConnection )
+# doQuery( myConnection )
+writeData(myConnection)
+myConnection.commit()
 myConnection.close()
